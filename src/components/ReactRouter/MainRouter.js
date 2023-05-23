81b280d843cbe87da50525f6ada20c1fa94f6233
MUHAMMED IRSHAD K <mialungal@gmail.com>
2023-05-23 19:13:25 +0530

    first commit

---
+++ src/components/ReactRouter/MainRouter.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import './MainRouter.css';
+import { Home } from './Pages/Home';
+import { About } from './Pages/About';
+import { Contact } from './Pages/Contact';
+import { Link, Route, Routes } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+export const MainRouter = () => {
+  return (
+    <div>
+        <>
+        <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="">
+            <ul>
+                <li>
+                    <Link to="/">Home</Link> 
+                </li>
+                <li>
+                    <Link to="about">About</Link> 
+                </li>
+                <li>
+                    <Link to="contact">Contact</Link> 
+                </li>
+                
+            </ul>
+            </Nav>
+        </Container>
+      </Navbar>
+        </>
+        <Routes>
+            <Route path="/" element={<Home />}/>
+            <Route path="about" element={<About />}/>
+            <Route path="contact" element={<Contact />}/>
+        </Routes>
+    </div>
+  )
+}
---
+++ src/components/ReactRouter/Pages/Home.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+export const Home = () => {
+  return (
+    <div>
+    <div class="row">
+  <div class="col-8">
+    <br></br>
+    <h2>
+    Hello, my name is <b>Muhammed Irshad</b>
+    </h2>
+    <h4>
+
+    </h4>
+    <p>
+    To build a career in the field of mechanical engineering and to explore me fully with which I can increase my potential to work effectively in the field of mechanical engineering. I would like to constantly upgrade my knowledge and skills with every opportunity.
+
+
+    </p>
+  </div>
+  <div class="col-4">
+    <img src='/src/components/ReactRouter/Pages/My photo.jpg'/>
+  </div>
+</div>
+    </div>
+  )
+}
